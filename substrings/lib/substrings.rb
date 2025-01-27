55e425066f531b3507c5087bce6c38fc13e4ea19
AdrianStymne <163299691+AdrianStymne@users.noreply.github.com>
2025-01-27 12:37:00 +0000

    Add same_string method to substrings class
    To develop basic logic for theubstrings method,
    added a method that counts the number of strings
    that are the same as the input string

--- substrings/lib/substrings.rb
+++ substrings/lib/substrings.rb
@@ -1,4 +1,11 @@
 class Substrings
-    def which_substrings(string, dictionary)
+    def same_string(string, dictionary)
+        dictionary.reduce(Hash.new(0)) do |count, word|
+            if word == string 
+                count[word] +=1
+            end
+        count
+        end
+
     end
 end
--- substrings/spec/substrings_spec.rb
+++ substrings/spec/substrings_spec.rb
@@ -2,17 +2,17 @@ require 'substrings'
 describe 'substrings' do
    it 'takes a word and a dictionary, and returns one if there is one' do
       s = Substrings.new
-      indices = s.which_substrings('low', ['low','carbon'])
-      indices.to eq({"low" => 1})
+      results = s.same_string('low', ['low','carbon'])
+      expect(results).to eq({"low" => 1})
    end
-   xit 'takes a word and a dictionary, and returns none if there is none' do
+   it 'takes a word and a dictionary, and returns none if there is none' do
       s = Substrings.new
-      indices = s.which_substrings('low', ['low','carbon'])
-      indices.to eq(nil)
+      results = s.same_string('low', ['high','carbon'])
+      expect(results).to eq({})
    end
-   xit 'takes a word and a dictionary, and returns more than one if there is more than one' do
+   it 'takes a word and a dictionary, and returns more than one if there is more than one' do
       s = Substrings.new
-      indices = s.which_substrings('low', ['low','carbon', 'low', 'low'])
-      indices.to eq({"low" => 3})
+      results = s.same_string('low', ['low','carbon', 'low', 'low'])
+      expect(results).to eq({"low" => 3})
    end
 end
