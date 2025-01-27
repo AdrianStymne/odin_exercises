a85cdb9aa223486e2441e872d7edbbe245dc969d
AdrianStymne <163299691+AdrianStymne@users.noreply.github.com>
2025-01-27 17:50:51 +0000

    Implement Rubocop and linting
    Used Rubocop to implement linting imrpovements, particularly across the
    caesar cipher project

--- Caesar_cipher/main.rb
+++ Caesar_cipher/main.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 require_relative 'lib/caesar'
 require 'rubocop'
 c = Caesar.new
-puts c.encrypt("yoho ho and a bottle of rum")
+puts c.encrypt('yoho ho and a bottle of rum')
--- Caesar_cipher/spec/caesar_spec.rb
+++ Caesar_cipher/spec/caesar_spec.rb
@@ -1,38 +1,46 @@
+# frozen_string_literal: true
+
 require 'caesar'
 describe 'Caesar' do
-    it 'shifts a single letter by five steps' do
-        c = Caesar.new
-        shifted = c.encrypt('a')
-        expect(shifted).to eq('f')
-        end
-    it 'shifts a string of letters by five steps' do
-        c = Caesar.new
-        shifted = c.encrypt('ab')
-        expect(shifted).to eq('fg')
-        end
-    it 'wraps around z' do
-        c = Caesar.new
-        shifted = c.encrypt('xz')
-        expect(shifted).to eq('ce')
-        end
-    it 'can take upcase and downcase and return the same' do
-        c = Caesar.new
-        shifted = c.encrypt('AbC')
-        expect(shifted).to eq('FgH')
-        end
-    it 'shifts letters by any number of steps' do
-        c = Caesar.new
-        shifted = c.encrypt('AbC', 7)
-        expect(shifted).to eq('HiJ')
-        end
-    it 'maintains special characters' do
-            c = Caesar.new
-            shifted = c.encrypt('A bC!', 7)
-            expect(shifted).to eq('H iJ!')
-            end
-    it 'returns error for any non-string' do
+  it 'shifts a single letter by five steps' do
     c = Caesar.new
-            shifted = c.encrypt('A bC!', 7)
-            expect {c.encrypt(123, 7) }.to raise_error("Not a string!")
-            end
+    shifted = c.encrypt('a')
+    expect(shifted).to eq('f')
+  end
+
+  it 'shifts a string of letters by five steps' do
+    c = Caesar.new
+    shifted = c.encrypt('ab')
+    expect(shifted).to eq('fg')
+  end
+
+  it 'wraps around z' do
+    c = Caesar.new
+    shifted = c.encrypt('xz')
+    expect(shifted).to eq('ce')
+  end
+
+  it 'can take upcase and downcase and return the same' do
+    c = Caesar.new
+    shifted = c.encrypt('AbC')
+    expect(shifted).to eq('FgH')
+  end
+
+  it 'shifts letters by any number of steps' do
+    c = Caesar.new
+    shifted = c.encrypt('AbC', 7)
+    expect(shifted).to eq('HiJ')
+  end
+
+  it 'maintains special characters' do
+    c = Caesar.new
+    shifted = c.encrypt('A bC!', 7)
+    expect(shifted).to eq('H iJ!')
+  end
+
+  it 'returns error for any non-string' do
+    c = Caesar.new
+    c.encrypt('A bC!', 7)
+    expect { c.encrypt(123, 7) }.to raise_error('Not a string!')
+  end
 end
