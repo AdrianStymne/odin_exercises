ae2e4030a240942816c90bf81989499c59d42a77
AdrianStymne <163299691+AdrianStymne@users.noreply.github.com>
2025-01-27 13:40:09 +0000

    Completed substrings exercise

--- substrings/lib/substrings.rb
+++ substrings/lib/substrings.rb
@@ -1,11 +1,19 @@
 class Substrings
-    def same_string(string, dictionary)
-        dictionary.reduce(Hash.new(0)) do |count, word|
-            if word == string 
-                count[word] +=1
-            end
-        count
+    def substrings(string, dictionary)
+        str_array = string.split
+        str_array.reduce(Hash.new(0)) do |result, part_of_string |
+            result.merge(compare_string_with_dict(part_of_string, dictionary)) { 
+                |_key, old_val, new_val| old_val + new_val }
         end
 
+    end
+    def compare_string_with_dict(string, dictionary)
+        dictionary.reduce(Hash.new(0)) do |count, word|
+            if string.downcase.include? word
+                count[word] +=1
+            
+            end
+            count 
+        end
     end
 end
--- substrings/spec/substrings_spec.rb
+++ substrings/spec/substrings_spec.rb
@@ -2,17 +2,55 @@ require 'substrings'
 describe 'substrings' do
    it 'takes a word and a dictionary, and returns one if there is one' do
       s = Substrings.new
-      results = s.same_string('low', ['low','carbon'])
+      results = s.substrings('low', ['low','carbon'])
       expect(results).to eq({"low" => 1})
    end
    it 'takes a word and a dictionary, and returns none if there is none' do
       s = Substrings.new
-      results = s.same_string('low', ['high','carbon'])
+      results = s.substrings('low', ['high','carbon'])
       expect(results).to eq({})
    end
-   it 'takes a word and a dictionary, and returns more than one if there is more than one' do
+   
+   it 'takes a word and a dictionary, and returns one substring if there is one' do
       s = Substrings.new
-      results = s.same_string('low', ['low','carbon', 'low', 'low'])
-      expect(results).to eq({"low" => 3})
+      results = s.substrings('below', ['low','carbon'])
+      expect(results).to eq({"low" => 1})
+   end
+   it 'takes a word and a dictionary, and returns no substring if there is none' do
+      s = Substrings.new
+      results = s.substrings('below', ['high','carbon'])
+      expect(results).to eq({})
+   end
+   it 'handles both string and substrings' do
+      s = Substrings.new
+      dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+      results = s.substrings("below", dictionary)
+      expect(results).to eq({ "below" => 1, "low" => 1 })
+   end
+   it 'handles capitalization' do
+      s = Substrings.new
+      dictionary = ["below","howdy","it"]
+      results = s.substrings("Howdy", dictionary)
+      expect(results).to eq({"howdy" => 1})
+   end
+   it 'handles more than one word' do
+      s = Substrings.new
+      dictionary = ["below","howdy","it"]
+      results = s.substrings("howdy it", dictionary)
+      expect(results).to eq({"howdy" => 1, "it" => 1})
+   end
+
+   it 'can count the same string for multiple words' do
+      s = Substrings.new
+      dictionary = ["it","i","sit"]
+      results = s.substrings("I sit! it?", dictionary)
+      expect(results).to eq({"it" => 2, "i" => 3,"sit" => 1 })
+   end
+   it 'handles the example' do
+      s = Substrings.new
+      dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+      results = s.substrings("Howdy partner, sit down! How's it going?", dictionary)
+      expect(results).to eq({ "down" => 1, "go" => 1, "going" => 1, 
+      "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 })
    end
 end
