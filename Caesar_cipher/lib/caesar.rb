9fd4d1e28efe33d2d0758cfe62245311bf56325c
AdrianStymne <163299691+AdrianStymne@users.noreply.github.com>
2025-01-27 11:36:35 +0000

    Initial commit

---
+++ Caesar_cipher/lib/caesar.rb
@@ -0,0 +1,26 @@
+class Caesar
+    ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.chars
+    ALPHABET_LENGTH = ALPHABET.length
+    def encrypt(unencrypted_string, shift = 5)
+        raise StandardError, "Not a string!" unless unencrypted_string.is_a?(String)
+        unencrypted_string.chars.reduce('') do |encrypted_string, char|
+            if char.match?(/[A-Za-z]/)
+                encrypted_string + shift_letter(char,shift)
+            else
+                encrypted_string + char
+            end
+        end
+    end
+    def shift_letter(char,shift)
+        char_was_upcase = ('A'..'Z').include? char
+        char_index = ALPHABET.find_index(char.downcase)
+        shifted_index = (char_index + shift) % ALPHABET_LENGTH
+        if char_was_upcase
+            encrypted_char = ALPHABET[shifted_index].upcase
+        else
+            encrypted_char = ALPHABET[shifted_index]
+        end
+    end
+end
+
+
